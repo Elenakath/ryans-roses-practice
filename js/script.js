622e03132f195b644ea7190605ce870be1ef00c4
elena_e <elenaka123@gmail.com>
2024-03-26 13:40:14 -0500

    inital commit

---
+++ js/script.js
@@ -0,0 +1,17 @@
+var footer = document.querySelector(".contact");
+footer.style.backgroundColor = "#d7ddf3";
+
+console.log(footer);
+
+var tagline = document.querySelector(".tagline");
+tagline.innerText = "To infinity and beyond!"
+
+console.log(tagline);
+
+var address = document.querySelector("address");
+address.style.lineHeight = "1.5em";
+address.style.fontSize = "1.75em";
+
+var everyDay = document.querySelector("h3");
+everyDay.innerHTML = "We're here for you <em>every day</em> of the week.";
+console.log(everyDay);
